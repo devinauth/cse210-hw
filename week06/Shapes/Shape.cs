4e25afe6e0e27ff635de63ee49280f365730fcd6
Devi Nauth <https://github.com/devinauth>
2025-02-10 17:09:33 -0400

    changes

---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,16 @@
+public abstract class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public abstract double GetArea(); // Abstract method to be implemented in derived classes
+}
